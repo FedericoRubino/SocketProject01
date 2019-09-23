bb7e0f6bdf3b3c0aa5a425d4cd747083732c4378
Federico Rubino <52460126+FedericoRubino@users.noreply.github.com>
2019-09-23 15:50:37 -0700

    Update UDP_Ping_Client.3.py

--- Ping/UDP_Ping_Client.3.py
+++ Ping/UDP_Ping_Client.3.py
@@ -38,3 +38,67 @@ for i in range(10):
 #Need to add calculations
 	RTT = float((int(time.time()*1000)-int(timing))/1000)
 	print (str(sequence_num) + " " + str(RTT)) 
+
+	
+	
+	
+	
+'''
+New version: 9/23/2019
+
+Python3
+UDP_Pinger_Client.py
+'''
+
+#IMPORTS (Timing library and socket for network)
+import time
+from socket import*
+
+serverName ='127.0.0.1'
+serverPort = 12000
+
+#Creating a Socket object that accepts an address and datagram
+clientSocket = socket(AF_INET,SOCK_DGRAM)
+
+#Giving the socket a timeout of 1 so that it doesn't wait forever
+clientSocket.settimeout(1)
+
+#For each i (ten total) send a 'ping' packet
+#Its format is 'sequence# timing#'
+for i in range(10):
+    the_time = int(time.time()*1000)
+    the_time02 = time.time()*1000
+
+    message = str(i) + " " + str(the_time)
+
+    #Socket sends message to address+port#
+    clientSocket.sendto(message.encode(),(serverName, serverPort))
+
+    #Using try/except to detect timeout
+    #It will listen for a message and store the address its from until
+    # either it gets the message or timesout
+    try:
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    except timeout as e:
+        print (e)
+        continue
+
+    #So long as it doesn't timeout it should take the datagram, decode it, split
+    # it at the space by its sequence number and timing
+    sequence_num, timing = modifiedMessage.decode().split()
+    #Would be nice to format the numbers to resemble what they should
+    #Need to add calculations
+
+    RTT = float((int(time.time()*1000)-int(timing))/1000)
+
+    print("RTT:", RTT)
+    var = '%.5f' % ((time.time()*1000 - the_time02)/1000)
+
+
+
+    print("My version:", var)
+
+
+    print (str(sequence_num) + " " + str(RTT))
+
+	
