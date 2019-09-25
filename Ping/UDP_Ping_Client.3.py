37ca1468c937cde0678ded96df9e8d3dcb4fa516
Federico Rubino <52460126+FedericoRubino@users.noreply.github.com>
2019-09-25 15:27:46 -0700

    Update UDP_Ping_Client.3.py

--- Ping/UDP_Ping_Client.3.py
+++ Ping/UDP_Ping_Client.3.py
@@ -37,10 +37,6 @@ rate(in percentage).
 
 """
 
-
-
-
-
 #IMPORTS (Timing library and socket for network and math functions)
 import time
 from socket import*
@@ -54,20 +50,29 @@ serverPort = 12000
 # list to be used for tracking RTTs
 RTT_times = [(-1) for _ in range(10)]
 
-
-
 #Creating a Socket object that accepts an address and datagram
 clientSocket = socket(AF_INET,SOCK_DGRAM)
 
 #Giving the socket a timeout of 1 so that it doesn't wait forever
 clientSocket.settimeout(1)
 
+# Variables for EstimatedRTT and DEVT
+EstimatedRTT = 0.0
+DevRTT = 0.0
+alpha = 0.125
+betta = 0.25
+
 #For each i (ten total) send a 'ping' packet
 #Its format is 'sequence# timing#'
+print()
+print("CST311 Programming Assignment #1\nby Tim Hanneman & Federico Rubino")
+print()
+print("Pings:")
+print("------")
 for i in range(10):
     the_time = time.time()*1000
 
-    message = str(i) + " " + str(the_time)
+    message = str(i) + " " + str(the_time) + " message"
 
     #Socket sends message to address+port#
     clientSocket.sendto(message.encode(),(serverName, serverPort))
@@ -78,37 +83,41 @@ for i in range(10):
     try:
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
     except timeout as e:
-        print (e)
+        print ("Request timed out")
         continue
 
     # So long as it doesn't timeout it should take the datagram,
     # decode it, split
     # it at the space by its sequence number and timing
-    sequence_num, timing = modifiedMessage.decode().split()
-    #Would be nice to format the numbers to resemble what they should
-    #Need to add calculations
+    sequence_num, timing, msg = modifiedMessage.decode().split()
 
     # Roundtrip time is calculated by finding the difference between the
     # time before the message was sent and after it was received
-    #RTT = float((int(time.time()*1000)-int(timing))/1000)
-    RTT = '%.5f' % ((time.time()*1000 - the_time)/1000)
+    sampleRTT = ((time.time()*1000 - the_time)/1000)
+    RTT = '%.5f' % sampleRTT
+
+    # Calculate the EstimatedRTT and the DevRTT
+    EstimatedRTT = (1.0 - alpha) * EstimatedRTT + (alpha * sampleRTT)
+    DevRTT = (1.0 - betta) * DevRTT + (betta * abs(sampleRTT - EstimatedRTT))
+
+
     RTT_times[i] = float(RTT)
-    print ("Ping Number:",str(sequence_num),"RTT:", str(RTT))
+    print ("Ping Number:",str(sequence_num),"RTT:", str(RTT), "Received message:", msg)
 
-
+# makese a list out of RTT_times that are larger than 0
+# In other words this list only has the successful RTTs
 found_packages = list(filter(lambda x: x > 0,RTT_times))
+
 print()
+print("RTT statistics:")
+print("---------------")
 print("Package loss rate: "+ str((10-len(found_packages))/10 *100) + "%")
-print()
 print("min:",'%.5f' % min(found_packages))
 print("max:",'%.5f' % max(found_packages))
 print("mean:",'%.5f' % statistics.mean(found_packages))
 print()
-print("Suggested timerate is:", '%.5f' % (max(found_packages)*100)) 
-
-# after a 1000 runs we got:
-#min: 0.00004
-#max: 0.01010  # this might have to be the suggested timerate
-#mean: 0.00045
-
+print("Estimated Roundtrip time",'%.5f' % ( EstimatedRTT))
+print("Deviation Roundtrip time:",'%.5f' % ( DevRTT))
+print("Suggested Timeout Interval is:", '%.5f' % (EstimatedRTT + 4*DevRTT))
+print()
 
